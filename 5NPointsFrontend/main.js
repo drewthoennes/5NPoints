6207f48ff84af90ba898ff44fb74468e80e35ec1
drewthoennes <dthoenne@purdue.edu>
2017-11-26 16:22:56 -0500

    Removing unnecessary dependencies

--- 5NPointsFrontend/main.js
+++ 5NPointsFrontend/main.js
@@ -1,7 +1,6 @@
 var express = require('express'),
 	path = require('path'),
-	serveStatic = require('serve-static'),
-	history = require('connect-history-api-fallback')
+	serveStatic = require('serve-static')
 
 app = express();
 app.use(express.static(__dirname + "/dist"));
@@ -9,7 +8,6 @@ app.use('/points', express.static(__dirname + "/dist"));
 app.use('/rewards', express.static(__dirname + "/dist"));
 app.use('/login', express.static(__dirname + "/dist"));
 app.use('/admin', express.static(__dirname + "/dist"));
-app.use(history());
 
 var port = process.env.PORT || 8080;
 app.listen(port);
